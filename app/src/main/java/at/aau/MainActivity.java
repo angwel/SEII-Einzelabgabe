1194eb593597ba2b383564a99a6c25900b0409a2
Angelina <anwelwich@edu.aau.at>
2019-03-20 12:37:40 +0100

    Connected Layout to code

--- app/src/main/java/at/aau/MainActivity.java
+++ app/src/main/java/at/aau/MainActivity.java
@@ -2,17 +2,37 @@ package at.aau;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText matrikelnummer;
+    TextView response;
+    Button btnAbschicken;
+    Button btnAscii;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Connect objects
+        matrikelnummer = findViewById(R.id.input);
+        response = findViewById(R.id.response);
+        btnAbschicken = findViewById(R.id.btnAbschicken);
+        btnAscii = findViewById(R.id.btnAscii);
 
+        // Button Abschicken:
+        btnAbschicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-
+            }
+        });
     }
 }
